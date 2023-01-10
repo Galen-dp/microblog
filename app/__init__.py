da698ebf6feccb279617f4307850f3cac23e6c9d
Doug Peterson <doug@notre-chateau.com>
2023-01-10 16:21:29 -0700

    Added forms logic

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # This import is at the bottom of the file to prevent a circular import
 # situation with flask.
